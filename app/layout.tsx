98002eef58dd36c430bf0a2e9e00ee9e9419c5e7
Ryan Parag <parag.ryan@gmail.com>
2025-12-01 16:41:19 -0500

    fix head

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Timezones',
   description: 'View and manage timezones across the world',
+  url: 'https://timezones.ryanparag.com'
 }
 
 export default function RootLayout({
@@ -13,22 +14,28 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
 
+  const meta: {
+    title: 'Timezones',
+    description: 'View and manage timezones across the world',
+    url: 'https://timezones.ryanparag.com'
+  }
+
   return (
       <html lang="en">
         <Head>
           <link rel="icon" type="image/svg+xml" href="/favicon.svg"/>
           <link rel="icon" type="image/png" href="/favicon.png"/>
-          <meta name="title" content={metadata.title}/>
-          <meta name="description" content={metadata.description}/>
+          <meta name="title" content={meta.title}/>
+          <meta name="description" content={meta.description}/>
           <meta property="og:type" content="website"/>
-          <meta property="og:url" content={'https://timezones.ryanparag.com'}/>
-          <meta property="og:title" content={metadata.title}/>
-          <meta property="og:description" content={metadata.description}/>
+          <meta property="og:url" content={meta.url}/>
+          <meta property="og:title" content={meta.title}/>
+          <meta property="og:description" content={meta.description}/>
           <meta property="og:image" content="/og-image.png"/>
           <meta property="twitter:card" content="summary_large_image"/>
-          <meta property="twitter:url" content={'https://timezones.ryanparag.com'}/>
-          <meta property="twitter:title" content={metadata.title}/>
-          <meta property="twitter:description" content={metadata.description}/>
+          <meta property="twitter:url" content={meta.url}/>
+          <meta property="twitter:title" content={meta.title}/>
+          <meta property="twitter:description" content={meta.description}/>
           <meta property="twitter:image" content="/og-image.png"/>
           <meta name="keywords" content="timezones, world clock, ryan, parag, graphic, web, designer, product"/>
           <meta name="robots" content="index, follow"/>
